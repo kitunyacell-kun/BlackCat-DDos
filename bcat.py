aad8c4428c02e8cfa5332c25a319d21a5bf3de99
kitunyacell-kun <kitunyacell@gmail.com>
2026-01-22 21:13:30 +0700

    Update bcat.py

--- bcat.py
+++ bcat.py
@@ -67,12 +67,11 @@ def print_status():
     thread_num_mutex.acquire(True)
 
     thread_num += 1
-    #print the output on the sameline
+    print("")
     sys.stdout.write(f"\r\033[102m\033[30m{time.ctime().split( )[3]}\033[0m")
-    print("\033[38;5;57m Get-started\033[0m \033[33" +str(host)+ " \033[32mIp-asdress \033[38;5;206m" +ip+ "\033[0m")
     sys.stdout.flush()
+    print("")
     sys.stdout.write(f"\r*\033[103m[{str(thread_num)}]\033[0m")
-    print("\033[37m Get-started\033[0m \033[33m" +str(host)+ " \033[95mIp-address \033[37m" +ip+ "\033[0m")
     sys.stdout.flush()
     thread_num_mutex.release()
 
